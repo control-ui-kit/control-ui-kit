cdb05ba43665ab3fa8809f3197255c8437cd6bc6
Chris Chambers <chris@label-worx.com>
2021-02-26 16:48:39 +0000

    UrlManipulation helper and tests

---
+++ tests/Helpers/UrlManipulationTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Helpers;
+
+use ControlUIKit\Helpers\UrlManipulation;
+use Orchestra\Testbench\TestCase;
+
+class UrlManipulationTest extends TestCase
+{
+    /** @test */
+    public function a_basic_url_can_be_appended(): void
+    {
+        $url = 'https://example.com';
+        $query = 'new=parameter';
+        $expected = 'https://example.com?new=parameter';
+
+        self::assertSame($expected, (new UrlManipulation)->url($url)->append($query));
+    }
+
+    /** @test */
+    public function a_parameter_url_can_be_appended(): void
+    {
+        $url = 'https://example.com?previous=option';
+        $query = 'new=parameter';
+        $expected = 'https://example.com?previous=option&new=parameter';
+
+        self::assertSame($expected, (new UrlManipulation)->url($url)->append($query));
+    }
+
+    /** @test */
+    public function an_anchor_url_can_be_appended(): void
+    {
+        $url = 'https://example.com#anchor';
+        $query = 'new=parameter';
+        $expected = 'https://example.com?new=parameter#anchor';
+
+        self::assertSame($expected, (new UrlManipulation)->url($url)->append($query));
+    }
+
+    /** @test */
+    public function a_parameter_anchor_url_can_be_appended(): void
+    {
+        $url = 'https://example.com?previous=option#anchor';
+        $query = 'new=parameter';
+        $expected = 'https://example.com?previous=option&new=parameter#anchor';
+
+        self::assertSame($expected, (new UrlManipulation)->url($url)->append($query));
+    }
+
+    /** @test */
+    public function a_parameter_anchor_url_can_be_appended_with_anchor(): void
+    {
+        $url = 'https://example.com?previous=option#anchor';
+        $query = 'new=parameter';
+        $anchor = 'new-anchor';
+        $expected = 'https://example.com?previous=option&new=parameter#new-anchor';
+
+        self::assertSame($expected, (new UrlManipulation)->url($url)->withAnchor($anchor)->append($query));
+    }
+
+    /** @test */
+    public function a_parameter_url_can_be_appended_with_anchor_while_passing_in_empty_anchor(): void
+    {
+        $url = 'https://example.com?previous=option#anchor';
+        $query = 'new=parameter';
+        $anchor = '';
+        $expected = 'https://example.com?previous=option&new=parameter#anchor';
+
+        self::assertSame($expected, (new UrlManipulation)->url($url)->withAnchor($anchor)->append($query));
+    }
+
+    /** @test */
+    public function a_parameter_url_can_be_appended_with_anchor_while_passing_in_null_anchor(): void
+    {
+        $url = 'https://example.com?previous=option#anchor';
+        $query = 'new=parameter';
+        $anchor = null;
+        $expected = 'https://example.com?previous=option&new=parameter#anchor';
+
+        self::assertSame($expected, (new UrlManipulation)->url($url)->withAnchor($anchor)->append($query));
+    }
+}
