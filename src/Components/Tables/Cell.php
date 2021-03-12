2a57e850fcf612830d0e5e4e9545da3c4a5b1d2b
Chris Chambers <chris@label-worx.com>
2021-03-12 13:53:56 +0000

    Clean up

--- src/Components/Tables/Cell.php
+++ src/Components/Tables/Cell.php
@@ -116,16 +116,4 @@ class Cell extends Component
 
         throw new ControlUIKitException('Formatter does not exist for ['.$formatter.']');
     }
-
-    private function appendDisplay($style)
-    {
-        $classes = explode(" ", $style);
-
-
-        $displayOptions = [ 'inline', 'block', 'contents', 'flex', 'grid', 'inline-block', 'inline-flex', 'inline-grid',
-            'inline-table', 'list-item', 'run-in', 'table', 'table-caption', 'table-column-group',
-            'table-header-group', 'table-footer-group', 'table-row-group', 'table-cell', 'table-column', 'table-row',
-            'none', 'initial', 'inherit' ];
-//        if ()
-    }
 }
